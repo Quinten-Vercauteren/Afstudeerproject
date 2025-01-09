3f555b2a5c9f26f9ae3f0b5312867e9774fcd8a2
Quinten Vercauteren <quinten.vercauteren@telenet.be>
2025-01-09 11:18:27 +0000

    dubble insert fix

--- models.py
+++ models.py
@@ -16,7 +16,8 @@ class FilamentData(Base):
     operation = Column(String)
 
 # Database connection setup
-DATABASE_URL = "mariadb+mariadbconnector://root:AfstudeerOcto123?@localhost:3306/filament_weight"
+DATABASE_USER_SECRET = "PyMariaDB123"
+DATABASE_URL = f"mariadb+mariadbconnector://python:{DATABASE_USER_SECRET}@localhost:3306/filament_weight"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -25,17 +26,23 @@ def add_data(cur, time, weight, operation):
     cur.execute("INSERT INTO filament_weight.filament_data (time, weight, operation) VALUES (?, ?, ?)", 
                 (time, weight, operation))
 
+def get_last_data(cur):
+    cur.execute("SELECT time FROM filament_weight.filament_data ORDER BY time DESC LIMIT 1") # Get the time of the last row
+    return cur.fetchone()[0] # Return the time of the last row
+
+
 # Instantiate Connection
 try:
    conn = mariadb.connect(
       host="localhost",
       port=3306,
-      user="root",
-      password="AfstudeerOcto123?",  # Ensure the password is correct
+      user="python",
+      password=DATABASE_USER_SECRET,  # Ensure the password is correct
       autocommit=True)
 except mariadb.Error as e:
     print(f"Error connecting to the database: {e}")
     sys.exit(1)
+
 
 # Instantiate Cursor
 cur = conn.cursor()
--- run.py
+++ run.py
@@ -14,7 +14,7 @@ import time
 import os
 import queue
 import shared_state  # Import shared state module without aliasing
-from models import add_data, conn, cur, SessionLocal  # Import add_data, conn, cur, and SessionLocal from models
+from models import add_data, conn, cur, SessionLocal, get_last_data  # Import add_data, conn, cur, SessionLocal, and get_last_data from models
 from shared_state import get_printer_status, set_printer_status, get_servicing_state, toggle_servicing_state, get_last_known_status, set_last_known_status
 
 # Initialize HX711 on startup
@@ -41,14 +41,14 @@ class PrinterState:
         self.last_printing_time = 0
 
 def db_trigger(current_status, printer_state):
-    print("------------------db_trigger-------------")
     current_time = time.time()
-    print(printer_state.last_known_status)
+
     if current_status != printer_state.last_known_status:
-        print(f"Status changed from {printer_state.last_known_status} to {current_status}")
+        printer_state.last_known_status = current_status
+        set_last_known_status(current_status)
         if current_status == "Printing" and printer_state.inactiveSwitch:
             printer_state.inactiveSwitch = False
-            if current_time - printer_state.last_insertion_time >= 10 and current_time - printer_state.last_printing_time >= 10:
+            if current_time - printer_state.last_insertion_time >= 100 and current_time - printer_state.last_printing_time >= 100:
                 weight = get_filament_weight()
                 log_event(f"Current filament weight: {weight} grams")
                 operation = "Started printing"
@@ -56,25 +56,28 @@ def db_trigger(current_status, printer_state):
                 printer_state.last_insertion_time = current_time
                 printer_state.last_printing_time = current_time
                 log_event("The printer started printing.")
-                print("The printer started printing.")
         elif current_status != "Printing" and not printer_state.inactiveSwitch:
             printer_state.inactiveSwitch = True
-            if current_time - printer_state.last_insertion_time >= 10:
+            if current_time - printer_state.last_insertion_time >= 100:
                 weight = get_filament_weight()
                 operation = "Stopped printing"
                 save_weight_data(weight, operation)
                 printer_state.last_insertion_time = current_time
                 log_event("The printer stopped printing.")
-                print("The printer stopped printing.")
-        printer_state.last_known_status = current_status
-        set_last_known_status(current_status)  # Write last known status to JSON
     print(printer_state.last_known_status)
 
 def save_weight_data(weight, operation):
     """Save the weight data in MariaDB."""
     time_str = time.strftime('%Y-%m-%d %H:%M:%S')  # Get current time in required format
-    add_data(cur, time_str, weight, operation)
-    log_event(f"Weight data saved: {weight} grams, {operation}")
+    last_insertion_time_dt = get_last_data(cur)  # Get the last insertion time as a datetime object
+    last_insertion_time = last_insertion_time_dt.timestamp()  # Convert to timestamp
+    current_time = time.mktime(time.strptime(time_str, '%Y-%m-%d %H:%M:%S'))
+    
+    if current_time - last_insertion_time >= 10:
+        add_data(cur, time_str, weight, operation)
+        log_event(f"Weight data saved: {weight} grams, {operation}")
+    else:
+        log_event("Data not saved. Less than 10 seconds since the last insertion.")
 
 def check_octoprint_connection():
     """Check if OctoPrint is available."""
