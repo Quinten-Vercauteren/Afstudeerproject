bcd61efc292cfadeaf0b9a12b651d1121a42e1e3
Quinten Vercauteren <quinten.vercauteren@telenet.be>
2025-01-13 09:53:29 +0000

    autostartup script added
    Former-commit-id: 78d74a711e957dff8b51bb00bea8173562ef545f

--- start_project.sh
+++ start_project.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source /home/pi/projectdir/Afstudeerproject/venv/bin/activate
-sudo python /home/pi/projectdir/Afstudeerproject/run.py
+# Activate the virtual environment
+source /home/Octo/projectdir/Afstudeerproject/venv/bin/activate
+# Run the Python script
+sudo python /home/Octo/projectdir/Afstudeerproject/run.py
