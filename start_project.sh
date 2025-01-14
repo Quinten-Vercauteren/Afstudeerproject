833c6826d1f1e4fc717f0407d899f3d9195fa51c
Quinten Vercauteren <quinten.vercauteren@telenet.be>
2025-01-14 13:54:02 +0000

    test push
    Former-commit-id: 47b43170cf146ab301063e1b8037a9eb467ff56a

--- start_project.sh
+++ start_project.sh
@@ -2,4 +2,4 @@
 # Activate the virtual environment
 source /home/Octo/projectdir/Afstudeerproject/venv/bin/activate
 # Run the Python script
-#sudo python /home/Octo/projectdir/Afstudeerproject/run.py
+sudo python /home/Octo/projectdir/Afstudeerproject/run.py
